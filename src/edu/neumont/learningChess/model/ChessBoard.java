b29b6b59344ba2f13e05d5a81f5006e4fb993f91
neuPanda <spainman0@yahoo.com>
2011-08-25 16:04:13 -0600

    Board and methods

--- src/edu/neumont/learningChess/model/ChessBoard.java
+++ src/edu/neumont/learningChess/model/ChessBoard.java
@@ -1,5 +1,19 @@
 package edu.neumont.learningChess.model;
 
+import edu.neumont.learningChess.api.Location;
+
 public class ChessBoard {
+
+	public static final int N_ROWS = 8;
+	public static final int N_COLS = 8;
+	
+	public ChessPiece getPiece(Location location) {
+		//TODO fill in body
+		return null;
+	}
+	
+	public void setPiece(ChessPiece piece, Location location) {
+		
+	}
 
 }
