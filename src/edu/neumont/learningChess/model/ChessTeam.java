820ed5d96408b8e30d97f57fe358e89cefadc9c1
neuPanda <spainman0@yahoo.com>
2011-08-25 16:32:54 -0600

    conflict fixed

--- src/edu/neumont/learningChess/model/ChessTeam.java
+++ src/edu/neumont/learningChess/model/ChessTeam.java
@@ -1,11 +1,5 @@
 package edu.neumont.learningChess.model;
 
-<<<<<<< HEAD
-=======
-import edu.neumont.learningChess.api.PieceType;
-import edu.neumont.learningChess.api.TeamColor;
->>>>>>> 847cf876eb39f25664892f20681c70c865405dea
-
 import java.util.List;
 
 import edu.neumont.learningChess.api.PieceType;
