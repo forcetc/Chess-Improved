753f876e147d10a685444f39200bf8b25e07e378
Isaac Anderson <phirephox4d@gmail.com>
2011-08-26 16:05:12 -0600

    added test class for Move Description

---
+++ src/edu/neumont/learningChess/tests/api/MoveDescriptionTest.java
@@ -0,0 +1,64 @@
+package edu.neumont.learningChess.tests.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoveDescriptionTest {
+
+	@Test
+	public void testMoveDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMoveCoordinates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMoveType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCapturedPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCapturedPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCastlingRookPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCastlingRookPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnPassantCapturedPawnLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEnPassantCapturedPawnLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPawnPromotionType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPawnPromotionType() {
+		fail("Not yet implemented");
+	}
+
+}
