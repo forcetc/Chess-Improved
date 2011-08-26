e73441b6568a53718ea389aad482b241617158dd
neuPanda <spainman0@yahoo.com>
2011-08-26 15:57:37 -0600

    team contains piece method filled in

--- src/edu/neumont/learningChess/model/ChessTeam.java
+++ src/edu/neumont/learningChess/model/ChessTeam.java
@@ -1,6 +1,7 @@
 package edu.neumont.learningChess.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import edu.neumont.learningChess.api.PieceType;
 import edu.neumont.learningChess.api.TeamColor;
@@ -12,10 +13,10 @@ public class ChessTeam {
 
 	public ChessPiece getUnusedPiece(PieceType type) {
 		ChessPiece piece = null;
-		
+
 		for (int i = 0; i < unusedChessPieces.size() && piece == null; i++) {
 			ChessPiece chessPiece = unusedChessPieces.get(i);
-			if(chessPiece.getType() == type) {
+			if (chessPiece.getType() == type) {
 				piece = chessPiece;
 			}
 		}
@@ -26,15 +27,31 @@ public class ChessTeam {
 		ChessPiece piece = null;
 		for (int i = 0; i < usedChessPieces.size() && piece == null; i++) {
 			ChessPiece chessPiece = usedChessPieces.get(i);
-			if(chessPiece.getType() == type) {
+			if (chessPiece.getType() == type) {
 				piece = chessPiece;
 			}
 		}
 		return piece;
 	}
 
-    public boolean containsPiece(ChessPiece chessPiece) {
-        //To change body of created methods use File | Settings | File Templates.
-        return false;
-    }
+	public boolean containsPiece(ChessPiece chessPiece) {
+		ChessPiece piece = null;
+		for (int i = 0; i < unusedChessPieces.size() && piece == null; i++) {
+			piece = unusedChessPieces.get(i);
+			if (chessPiece.equals(piece)) {
+				piece = chessPiece;
+			}
+		}
+		for (int i = 0; i < usedChessPieces.size() && piece == null; i++) {
+			piece = usedChessPieces.get(i);
+			if (chessPiece.equals(piece)) {
+				piece = chessPiece;
+			}
+		}
+		return piece!=null;
+	}
+
+	public Iterator<?> getPocibleMoves() {
+		return null;
+	}
 }
