3a4c8fd5247b135d37b6901cd0048b1048231eb9
Devon Moss <dvnmss@gmail.com>
2011-08-26 09:05:54 -0600

    modified MoveHistory class

--- src/edu/neumont/learningChess/model/MoveHistory.java
+++ src/edu/neumont/learningChess/model/MoveHistory.java
@@ -1,10 +1,36 @@
 package edu.neumont.learningChess.model;
 
+import java.util.Enumeration;
+import java.util.Stack;
+
 import edu.neumont.learningChess.api.MoveDescription;
 
-public class MoveHistory {
+public class MoveHistory implements Enumeration<MoveDescription>{
 	
+	private boolean lastMovePawnMovedTwo;
+	private Stack<MoveDescription> moves;
+	
+	public boolean isLastMovePawnMovedTwo() {
+		return lastMovePawnMovedTwo;
+	}
+
+	public void setLastMovePawnMovedTwo(boolean lastMovePawnMovedTwo) {
+		this.lastMovePawnMovedTwo = lastMovePawnMovedTwo;
+	}
+
 	public void push(MoveDescription moveDescription){
 		
+	}
+
+	@Override
+	public boolean hasMoreElements() {
+		return !moves.isEmpty();
+	}
+
+	@Override
+	public MoveDescription nextElement() {
+		MoveDescription moveDescription = moves.pop();
+		
+		return moveDescription;
 	}
 }
