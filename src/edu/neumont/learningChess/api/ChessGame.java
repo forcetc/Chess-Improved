b4fe7808a40bc6086a93b2a08ab7d1a49ad32df5
Don Matthews <don.matthews.iii@gmail.com>
2011-08-26 16:55:45 -0600

    Castling in its entirety

--- src/edu/neumont/learningChess/api/ChessGame.java
+++ src/edu/neumont/learningChess/api/ChessGame.java
@@ -1,11 +1,16 @@
 package edu.neumont.learningChess.api;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
 import edu.neumont.learningChess.model.ChessBoard;
 import edu.neumont.learningChess.model.ChessPiece;
 import edu.neumont.learningChess.model.ChessTeam;
+import edu.neumont.learningChess.model.King;
 import edu.neumont.learningChess.model.MoveHistory;
-
-import java.util.Enumeration;
+import edu.neumont.learningChess.model.Rook;
 
 public class ChessGame {
 	
@@ -124,13 +129,13 @@ public class ChessGame {
 
     private void setupTeam(ChessTeam team, char mainRow, char pawnRow) {
     	
-    	board.setPiece(team.getUnusedPiece(PieceType.KING), new Location(mainRow, 'e'));
-    	board.setPiece(team.getUnusedPiece(PieceType.QUEEN), new Location(mainRow, 'd'));
-    	board.setPiece(team.getUnusedPiece(PieceType.BISHOP), new Location(mainRow, 'c'));
-    	board.setPiece(team.getUnusedPiece(PieceType.BISHOP), new Location(mainRow, 'f'));
-    	board.setPiece(team.getUnusedPiece(PieceType.KNIGHT), new Location(mainRow, 'b'));
-    	board.setPiece(team.getUnusedPiece(PieceType.KNIGHT), new Location(mainRow, 'g'));
     	board.setPiece(team.getUnusedPiece(PieceType.ROOK), new Location(mainRow, 'a'));
+    	board.setPiece(team.getUnusedPiece(PieceType.KNIGHT), new Location(mainRow, 'b'));
+    	board.setPiece(team.getUnusedPiece(PieceType.BISHOP), new Location(mainRow, 'c'));
+    	board.setPiece(team.getUnusedPiece(PieceType.QUEEN), new Location(mainRow, 'd'));
+    	board.setPiece(team.getUnusedPiece(PieceType.KING), new Location(mainRow, 'e'));
+    	board.setPiece(team.getUnusedPiece(PieceType.BISHOP), new Location(mainRow, 'f'));
+    	board.setPiece(team.getUnusedPiece(PieceType.KNIGHT), new Location(mainRow, 'g'));
     	board.setPiece(team.getUnusedPiece(PieceType.ROOK), new Location(mainRow, 'h'));
 		for (int i = 0; i < ChessBoard.NUMBER_OF_COLUMNS; i++) {
 	    	board.setPiece(team.getUnusedPiece(PieceType.PAWN), new Location(pawnRow, 'a'+i));
@@ -165,7 +170,6 @@ public class ChessGame {
     }
 
     private Location getCurrentTeamsKingLocation() {
-
         ChessPiece king = getCurrentTeam().getUsedPiece(PieceType.KING);
         Location location = null;
         for (int row = 0; (row < ChessBoard.NUMBER_OF_ROWS) && (location == null); row++) {
@@ -180,22 +184,92 @@ public class ChessGame {
     }
 
     private boolean isPawnPromotion(Move move) {
-        return false;
+        // TOOD:
+    	return false;
     }
-
 
     private Location getCastleRookLocation(Move move) {
-
-        return null;
+    	Location rookLocation = null;
+    	
+    	King king = getPieceTypeAt(move.getFrom(), King.class);
+    	if (king != null && (!king.hasMoved() && isTwoHorizontalSpace(move))){
+    		Location tempRookLocation = suggestCastlingRookLocation(move);
+    		Rook rook = getPieceTypeAt(tempRookLocation, Rook.class);
+    		if(rook != null && !rook.hasMoved() && !board.piecesInPath(new Move(move.getFrom(),tempRookLocation)) && !isKingInCheckDuringMove(move) ){
+    			rookLocation = tempRookLocation;	
+    		}
+    	}
+        return rookLocation;
+    }
+    
+    private boolean isKingInCheckDuringMove(Move move) {
+    	boolean willBeInCheck = false;
+    	Iterator<Location> locations = move.getMoveLocations();
+    	ChessTeam otherTeam = currentTeamColor == TeamColor.LIGHT ? darkTeam : lightTeam;
+    	while(locations.hasNext() && !willBeInCheck) {
+    		Location location = locations.next(); 
+   			willBeInCheck = canAttack(otherTeam, location);
+    	}
+    	return willBeInCheck;
+	}
+    
+    private boolean isLegalMove(Move move) {
+    	return !board.piecesInPath(move) && targetLocationIsEmptyOrEnemy(move.getTo());
     }
 
-    private Location getEnPassantLocation(Move move) {
+	private boolean targetLocationIsEmptyOrEnemy(Location location) {
+		ChessTeam otherTeam = currentTeamColor == TeamColor.LIGHT ? darkTeam : lightTeam;
+		return board.getPiece(location) == null || otherTeam.containsPiece(board.getPiece(location));
+	}
+
+	private boolean canAttack(ChessTeam otherTeam, Location location) {
+		boolean canBeAttacked = false;
+		
+		Iterator<Move> moves = getLegalMoves(otherTeam);
+		while(moves.hasNext() && !canBeAttacked) {
+			Move move = moves.next();
+			canBeAttacked = move.getTo().equals(location);
+    	}
+		return canBeAttacked;
+	}
+
+	private Iterator<Move> getLegalMoves(ChessTeam otherTeam) {
+		List<Move> legalMoves = new ArrayList<Move>();
+		Iterator<Move> possibleMoves = otherTeam.getPossibleMoves();
+		for (Move move = possibleMoves.next(); possibleMoves.hasNext();) {
+			ChessPiece piece = board.getPiece(move.getFrom());
+			if(piece.isCapturingMove(move) && isLegalMove(move)) {
+				legalMoves.add(move);
+			}
+		}
+		return legalMoves.iterator();
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T extends ChessPiece> T getPieceTypeAt (Location location, Class<T> cls) {
+    	T returnedPiece = null;
+    	ChessPiece piece = board.getPiece(location);
+    	if ((piece != null) && (piece.getClass().equals(cls))) {
+    		returnedPiece = (T) piece;
+    	}
+    	return returnedPiece;
+    }
+
+    private Location suggestCastlingRookLocation(Move move) {
+    	return new Location(move.getFrom().getRow(), move.horizontalDistance() > 0 ? 7 : 0);
+	}
+
+	private boolean isTwoHorizontalSpace(Move move) {
+		return move.isHorizontal() && (Math.abs(move.horizontalDistance()) == 2);
+	}
+
+	private Location getEnPassantLocation(Move move) {
+		// TODO Auto-generated method stub
         return null;
     }
 
     public boolean isCaptureMove(Move move) {
+    	// TODO Auto-generated method stub
         return false;
     }
-
-
 }
