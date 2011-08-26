5c19efa49ea2f3b1966998050481047f8659e7f2
Bobby Zhou <bzhou@student.neumont.edu>
2011-08-26 12:34:59 -0600

    Added more code to unit test PieceDescriptionTest, code reviewrequired

--- src/edu/neumont/learningChess/tests/api/PieceDescriptionTest.java
+++ src/edu/neumont/learningChess/tests/api/PieceDescriptionTest.java
@@ -25,14 +25,40 @@ public class PieceDescriptionTest {
 
 	@Test
 	public void testGetColor() {
+		PieceDescription pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.KING);
+		assertEquals(TeamColor.LIGHT, pieceDesc.getColor());
+		
+		pieceDesc = new PieceDescription(TeamColor.DARK, true, PieceType.KING);
+		assertEquals(TeamColor.DARK, pieceDesc.getColor());
 	}
 
 	@Test
 	public void testHasMoved() {
+		PieceDescription pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.KING);
+		assertEquals(true, pieceDesc.hasMoved());
+		
+		pieceDesc = new PieceDescription(TeamColor.LIGHT, false, PieceType.KING);
+		assertEquals(false, pieceDesc.hasMoved());
 	}
 
 	@Test
 	public void testGetPieceType() {
+		PieceDescription pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.KING);
+		assertEquals(PieceType.KING, pieceDesc.getPieceType());
+		
+		pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.QUEEN);
+		assertEquals(PieceType.QUEEN, pieceDesc.getPieceType());
+		
+		pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.BISHOP);
+		assertEquals(PieceType.BISHOP, pieceDesc.getPieceType());
+		
+		pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.KNIGHT);
+		assertEquals(PieceType.KNIGHT, pieceDesc.getPieceType());
+		
+		pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.ROOK);
+		assertEquals(PieceType.ROOK, pieceDesc.getPieceType());
+		
+		pieceDesc = new PieceDescription(TeamColor.LIGHT, true, PieceType.PAWN);
+		assertEquals(PieceType.PAWN, pieceDesc.getPieceType());
 	}
-
 }
